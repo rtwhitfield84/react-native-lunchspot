e402a5c91af00b473776796d1a8f251a0151f263
Rico <richardthomaswhitfield@gmail.com>
2017-04-03 12:39:02 -0500

    added styles component

--- index.ios.js
+++ index.ios.js
@@ -6,12 +6,12 @@ import {
   View
 } from 'react-native';
 
-import { Examples } from '@shoutem/ui'; 
+import App from './components/App';
 
 export default class lunchspot extends Component {
   render() {
     return (
-      <Examples />
+      <App />
     );
   }
 }
